5127034eff9a60055dd6c16e3233e8270c6f4d0b
Pjotr Prins <pjotr.public01@thebird.nl>
2018-01-06 07:34:08 +0000

    Fix for ldc 1.6

--- mqm/qtl.d
+++ mqm/qtl.d
@@ -22,7 +22,7 @@ class SingleQTL{
       for(size_t m = 0; m < genotypes.length; m++){
         double[] w = newvector!double(phenotypes[0].length, 1.0);
         int[] nm   = newvector!int(1,1);
-        lodmatrix[p][m] = toLOD(mregression(createdesignmatrix(genotypes, m, geno_cov), phenotypes[p], w, nm, false));
+        lodmatrix[p][m] = toLOD(mregression(createdesignmatrix(genotypes, cast(int)m, geno_cov), phenotypes[p], w, nm, false));
       }
       if(verbose) write(".");
       stdout.flush();
@@ -44,7 +44,7 @@ double[][] createdesignmatrix(int[][] genotypes, int marker, int[] geno_cov = []
         if(v==(dm[i].length-1)){
           dm[i][v] = cast(double) genotypes[marker][i];
         }else{
-          uint cov = v - cast(uint) intercept;
+          uint cov = cast(uint)v - cast(uint) intercept;
           dm[i][v] = cast(double) genotypes[geno_cov[cov]][i];
         }
       }
